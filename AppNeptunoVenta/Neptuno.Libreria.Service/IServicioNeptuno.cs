6ba22a7e928444de2f97f1a73226780622d48083
Raúl Armas <raularmasbx@gmail.com>
2023-05-28 12:33:48 -0500

    first commit sample

---
+++ AppNeptunoVenta/Neptuno.Libreria.Service/IServicioNeptuno.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Neptuno.Libreria.Service
+{
+    public interface IServicioNeptuno <T>
+    {
+        #region Firmas
+        void create(SqlConnection c, T t);
+        void update(SqlConnection c, T t);
+        void delete(SqlConnection c, T t);
+        T FindForId(SqlConnection c, object t);
+        List<T> readAll(SqlConnection c);
+
+        #endregion
+    }
+
+    public interface IServicioNeptunoGenerico<T>
+    {
+        //definir las firmas
+        void create(T o);
+        void update(T o);
+        void delete(T o);
+        T findForId(int o);
+        List<T> readAll();
+
+    }
+}
